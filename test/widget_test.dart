1aeaa4e0b895424f8d4c17e38e4704df81eeae83
Vaishnavi <nagavaishnavipadala@email.com>
2026-09-05 13:55:49 +0530

    Fix expense tracker widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,10 @@ void main() {
   testWidgets('Expense Tracker app loads', (WidgetTester tester) async {
     await tester.pumpWidget(const ExpenseTrackerApp());
 
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.text('Expense Tracker'), findsOneWidget);
     expect(find.text('Add Expense'), findsOneWidget);
-    expect(find.text('Save Expense'), findsOneWidget);
   });
 }
+
